9d2007e6db91953512b0793594e133a2db471222
Justin <justindavidtrotta@gmail.com>
2022-05-20 14:03:54 -0400

    lookin good

--- src/app/render/render.component.ts
+++ src/app/render/render.component.ts
@@ -1,33 +1,30 @@
 import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, Input } from '@angular/core';
 import * as THREE from "three";
-
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 import Stats from 'three/examples/jsm/libs/stats.module'
+import * as Simplex from '@spissvinkel/simplex-noise';
+
 
 // Create THREE scene
 const scene = new THREE.Scene();
-scene.add(new THREE.AxesHelper(5))
 
 // Scene Lighting
 const light = new THREE.PointLight();
-light.position.set(0, 0, 13)
-light.intensity = 2
+light.position.set(30, 30, 50)
+light.intensity = 1
 scene.add(light);
-
 const ambinetLight = new THREE.AmbientLight();
 scene.add(ambinetLight);
 
 // Scene Camera
 const camera = new THREE.PerspectiveCamera(
-  75,
+  50,
   window.innerWidth / window.innerHeight,
   0.1,
-  1000
+  2000
 )
-
 camera.position.set(0, 8, 13)
-
 
 // Create & Initialize Renderer
 const renderer= new THREE.WebGLRenderer();
@@ -35,57 +32,56 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 // Scene Camera Controls
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.enableDamping = true
-controls.target.set(0, 1, 0)
+// const controls = new OrbitControls(camera, renderer.domElement)
+// controls.enableDamping = true
+// controls.target.set(0, 1, 0)
 
+// Procedural Terrain Generation
+//Simplex Noise setup
+const simplex = Simplex.mkSimplexNoise(Math.random)
 
-// Load FBX Model
-// const fbxLoader = new FBXLoader()
-// fbxLoader.load(
-//   '../assets/xbot.fbx',
-//   (object) => {
-//     object.traverse(function (child) {
-//       if ((child as THREE.Mesh).isMesh) {
-//         if ((child as THREE.Mesh).material) {
-//           ((child as THREE.Mesh).material as THREE.MeshBasicMaterial).transparent = false
-//         }
-//       }
-//     })
-//     scene.add(object)
-//   },
-//   (xhr) => {
-//     console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-//   },
-//   (error) => {
-//       console.log(error)
-//   }
-// )
+//Plane Setup
+function createPlane() {
+const side = 300
+const planeGeo = new THREE.PlaneGeometry(40, 40, side, side)
+const planeMat = new THREE.MeshPhysicalMaterial({
+  wireframe: false,
+  roughness: 0.5,
+  color: new THREE.Color("darkblue")
+});
+planeMat.reflectivity = 0
+planeMat.transmission = 1
+planeMat.roughness = 0.5
+planeMat.metalness = 0.05
+planeMat.clearcoat = 0.3
+planeMat.clearcoatRoughness = 0.25
+planeMat.color = new THREE.Color("darkblue")
+planeMat.ior = 1.2
+planeMat.thickness = 10.0
+let plane = new THREE.Mesh(planeGeo, planeMat);
+plane.castShadow = true;
+plane.receiveShadow = true;
 
-// Generate Terrain
-function createGround() {
-  const groundGeo = new THREE.PlaneGeometry(1000, 1000, 1500, 1500)
-  const displacementMap = new THREE.TextureLoader().load("../assets/difmap.jpeg")
-  const normalMap = new THREE.TextureLoader().load("../assets/NormalMap.jpg")
-  const colorMap = new THREE.TextureLoader().load("../assets/colormap.png")
+scene.add(plane);
 
-  const groundMat = new THREE.MeshStandardMaterial({
-    color: "white",
-    wireframe: false,
-    displacementMap: displacementMap,
-    displacementScale: 300,
-    normalMap: normalMap,
-    map: colorMap,
-    metalness: 0.2, 
-  })
-
-  let groundMesh = new THREE.Mesh(groundGeo, groundMat)
-  scene.add(groundMesh)
+return plane;
 }
 
-createGround();
+let plane = createPlane()
 
-scene.background = new THREE.Color("#87CEEB")
+// Update vertex with noise
+function vertexNoise(plane: THREE.Mesh, time: number){
+ 
+  let v3 = new THREE.Vector3();
+  for (let i = 0; i < plane.geometry.attributes['position'].count; i++){
+    v3.fromBufferAttribute(plane.geometry.attributes['position'], i).addScalar(time * 0.0001)
+    let z = time * 0.0000001
+    let noise = simplex.noise4D(v3.x * 0.1, v3.y, z, z)
+    plane.geometry.attributes['position'].setZ(i, noise);
+  }
+  plane.geometry.computeVertexNormals()
+  plane.geometry.attributes['position'].needsUpdate = true;
+}
 
 // Keep renderer same size as window
 window.addEventListener('resize', onWindowResize, false)
@@ -94,25 +90,22 @@ function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
   renderer.setSize(window.innerWidth, window.innerHeight)
-  render()
+  renderer.render(scene, camera);
 }
-const stats = Stats();
-document.body.appendChild(stats.dom)
 
-
-// Animation and render call
-function animate() {
+// Animate and render
+let animate = function () {
   requestAnimationFrame(animate)
 
-  controls.update()
+  let clock = new THREE.Clock();
+  clock.start();
+  let time = clock.startTime;
+  vertexNoise(plane, time)
 
-  render()
+   renderer.render(scene, camera);
 
-  stats.update();
-}
-
-function render() {
-  renderer.render(scene, camera)
+   
+   
 }
 
 animate()
